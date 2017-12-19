3de4ef42c029cbc385001b85b87154db09df7e8a
ronaldowl <32852206+ronaldowl@users.noreply.github.com>
2017-12-18 22:42:42 -0200

    Component Footer

---
+++ OscaApp/OscaApp/Models/Footer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OscaApp.Models
+{
+    public class Footer
+    {
+
+        public string nomeOrganizacao { get; set; }
+        public string msgAvaliacao { get; set; }
+
+    }
+}
---
+++ OscaApp/OscaApp/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OscaApp.Data;
+using OscaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OscaApp.ViewComponents
+{
+
+    [ViewComponent(Name = "Footer")]
+    public class FooterViewComponent : ViewComponent
+    {
+ 
+        private ContextPage contexto;
+
+        public FooterViewComponent( IHttpContextAccessor httpContext)
+        {      
+
+            this.contexto = new ContextPage(httpContext.HttpContext.Session.GetString("email"), httpContext.HttpContext.Session.GetString("organizacao"));
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            Footer modelo = new Footer();
+
+            modelo.nomeOrganizacao = this.contexto.organizacao;
+            modelo.msgAvaliacao = "Falta 20 dias de Avaliação";
+                
+
+            return View("LoginFooter", modelo);
+
+        }
+
+    }
+}
