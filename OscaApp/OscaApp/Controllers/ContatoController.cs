cf4fea4622e5a576ed78093c828ac1f226c08932
ronaldowl <32852206+ronaldowl@users.noreply.github.com>
2018-02-10 10:30:58 -0200

    contexto contato

--- OscaApp/OscaApp/Controllers/ContatoController.cs
+++ OscaApp/OscaApp/Controllers/ContatoController.cs
@@ -27,7 +27,8 @@ namespace OscaApp.Controllers
         public ContatoController(ContexDataService db, IHttpContextAccessor httpContext)
         {            
             this.contatoData = new ContatoData(db);           
-            this.contexto = new ContextPage(httpContext.HttpContext.Session.GetString("email"), httpContext.HttpContext.Session.GetString("organizacao"));
+            // this.contexto = new ContextPage(httpContext.HttpContext.Session.GetString("email"), httpContext.HttpContext.Session.GetString("organizacao"));
+            this.contexto = new ContextPage().ExtractContext(httpContext);
         }
 
         [HttpPost]
