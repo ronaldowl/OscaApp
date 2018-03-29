bdcbb5ebe54fa19e882dd593bfa74d5c20cbd58e
ronaldowl <32852206+ronaldowl@users.noreply.github.com>
2018-03-28 23:45:24 -0300

    Inclusão da plataforma de Report

---
+++ OscaApp/OscaApp/Controllers/Compartilhado/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OscaApp.Data;
+using OscaApp.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+
+namespace OscaApp.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ViewResult ReportNativo()
+        {
+            return View();
+        }
+    }
+}
---
+++ OscaApp/OscaReport/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OscaReport.Global" Language="C#" %>
